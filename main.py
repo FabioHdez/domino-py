ccfb2b4eb3ef37d2350a3b790331184920f5996b
Fabio Hernandez <44535804+fahio12@users.noreply.github.com>
2022-03-01 04:09:16 -0500

    starting play and display of pieces and table during each turn

--- main.py
+++ main.py
@@ -8,18 +8,45 @@ def main():
     random.shuffle(pieces)
 
     # Get input to get num of players (2 or 4)
-    playerNum = int(input("Please enter the number of players: "))
+    playerNum = int(input("Please enter the number of players (2 or 4): "))
 
     # Create players
     players = []
     for player in range(playerNum):
-        name = input(f"Enter player {player+1}'s name:")
+        name = input(f"Enter player {player+1}'s name: ")
         players.append(Player(name, pieces[:10]))
         del pieces[:10]
 
-    # Select wether playing with people or COMP
-    # ADD LATER
+    # ACTUAL GAME
+    table = []
+    end = False
+    while(end == False):
+        tableStatus = len(table)
+        for player in range(playerNum):
+            choice = "pass"
+            # THIS IF STATEMENT CHECKS IF THE TABLE IS EMPTY SO THAT THE FIRST
+            # PLAYER CAN START THE GAME
+            # Not ideal to check if table is empty every turn performance-wise
+            # but this way offers more concise code.
+            if not table:
+                print(f"{players[0].name} - pieces: ")
+                print(players[0].pieces)
+                choice = input(f"{players[0].name} - Choose a starting piece: ")
+                print (choice)
+                table.append(choice)
+                print(table)
+                print('-------------------------------------------------------')
 
+                continue
+
+            # Normal turn
+            print(table)
+            print(f"{players[player].name} - pieces: ")
+            print(players[player].pieces)
+            print('-------------------------------------------------------')
+
+        if tableStatus == len(table):
+            end = True
 
 #Creates a list with all the pieces in the game
 def createPieces():
@@ -31,11 +58,6 @@ def createPieces():
                 if piece == [j,i] and j != i:
                     pieces.remove(piece)
     return pieces
-
-# Gets the current pieces and selects the amount for each hand
-# def selectPieces(pieces,handSize):
-
-
 
 
 class Player:
